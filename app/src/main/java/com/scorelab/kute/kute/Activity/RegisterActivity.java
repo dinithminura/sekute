853b63534fb429f53e77806cb4f5e9e87112c35e
NickHub4017 <nrv.rox.u@gmail.com>
2017-02-05 00:49:47 +0530

    Done init of Register Activity.

---
+++ app/src/main/java/com/scorelab/kute/kute/Activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.scorelab.kute.kute.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nrv on 2/4/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+}
